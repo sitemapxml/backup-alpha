5662a0bc6cd424a47ccd7c7ac097c3f15cee54fa
sitemapxml <sitemapxml@protonmail.com>
2021-06-25 00:27:11 +0200

    Update functions.sh

--- includes/functions.sh
+++ includes/functions.sh
@@ -10,3 +10,18 @@
 # BACKING UP WEB APPLICATION
 # copying everything inside /var/www/
 # or everything inside definied webroot (apache or nginx conf) - add to roadmap
+
+fn_create_backup_directory () {
+  echo "Creating backup directory /var/uset-backups/"
+  sleep 1s
+  mkdir /var/uset-backups
+
+  echo "Creating backup archive directory "
+  # dprint=$( date "+%D" | sed 's/\//_/g' )
+
+  # Print date, hours and minutes
+  dprint=$( date "+%D-%H%M" | sed 's/\//_/g' )
+  dirname=$( echo "backup_$dprint" )
+
+  mkdir -p /var/uset-backups/$dirname
+}
